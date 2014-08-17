8122b4a766ad5ebb0c7897b8e4a9eae61cb78713
champ0007 <reachtushar@hotmail.com>
2014-08-17 12:42:28 +0530

    WebAPI + Dashboard

---
+++ AUDashboard/Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AUDashboard.Controllers
+{
+    public class DashboardController : ApiController
+    {
+        public int GetActiveProjects()
+        {
+            return 10;
+        }
+
+        public int GetOpenTasks()
+        {
+            return 20;
+        }
+
+        public int GetPendingInvoices()
+        {
+            return 30;
+        }
+
+        public int GetActiveResources()
+        {
+            return 40;
+        }
+    }
+}
