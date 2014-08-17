1e4b6d87b9143e0156d7d401c2cd068c17d36a5a
champ0007 <reachtushar@hotmail.com>
2014-08-17 12:20:56 +0530

    Revamp2

---
+++ AUDashboard/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AUDashboard.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ AUDashboard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AUDashboard.WebApiApplication" Language="C#" %>
