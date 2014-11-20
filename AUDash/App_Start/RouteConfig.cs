5ee6be0ec58b3bc6fbaa604838949777d318e384
champ0007 <reachtushar@hotmail.com>
2014-11-20 22:13:22 +0530

    TRying to rollback to stable version

--- AUDash/App_Start/RouteConfig.cs
+++ AUDash/App_Start/RouteConfig.cs
@@ -1,5 +1,4 @@
-﻿using AUDash.SessionManagement;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,7 +17,7 @@ namespace AUDash
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            ).RouteHandler = new SessionStateRouteHandler();
+            );
         }
     }
 }
--- AUDash/App_Start/WebApiConfig.cs
+++ AUDash/App_Start/WebApiConfig.cs
@@ -1,5 +1,4 @@
-﻿using AUDash.SessionManagement;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
